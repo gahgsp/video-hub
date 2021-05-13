501eed8843410d5353b59b71dd7809cb37992ef2
gahgsp <kelvinbianchi@gmail.com>
2021-05-13 18:46:58 -0300

    Added axios dependency to make requests to the Youtube API.

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const KEY = 'AIzaSyADAv8drFxDohlwtt4v37L1iNAmbJqodfM';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    key: KEY,
+    part: 'snippet',
+    maxResults: 5,
+  },
+});
